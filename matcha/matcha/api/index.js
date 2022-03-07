1ef16fc5d1880825ce115feb7ea60b8736e29b55
RobinMazars <45461136+RobinMazars@users.noreply.github.com>
2022-03-07 19:44:21 +0100

    connection to db postgres

--- matcha/matcha/api/index.js
+++ matcha/matcha/api/index.js
@@ -4,10 +4,19 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://postgres:changeme@postgres:5432/matcha')
+app.get("/test", (req, res) => {
+  console.log("log");
+  db.one('SELECT $1 AS value', 123)
+  .then((data) => {
+    console.log('DATA:', data.value)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+});
 
-app.get('/test', function (req, res) {
-  res.send('Test successfusdfl')
-})
 
 
 export default {
