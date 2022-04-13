3b72e51746795098cca131b49f0325442e55ad32
jobenass <57477996+jobenass@users.noreply.github.com>
2022-04-13 14:22:35 +0200

    home page

--- matcha/matcha/pages/index.vue
+++ matcha/matcha/pages/index.vue
@@ -1,22 +1,9 @@
 <template>
-  <div>
-    <p>Hello you have been successfully logged in</p>
-  </div>
+  <homePage />
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  methods: {
-    async getUserInfo() {
-      try {
-        await this.$axios.get('user');
-      } catch (e) {
-        this.errorMsg = e.response.data.msg;
-        this.alertStatus = true;
-        this.successStatus = false;
-      }
-    },
-  },
 };
 </script>
