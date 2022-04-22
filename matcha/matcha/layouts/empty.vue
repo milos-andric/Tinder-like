455b9178ee29bce06bdd9c6c804448e6cff6474e
Thais Marcon <tmarcon@z4r6p3.42lyon.fr>
2022-04-22 14:43:33 +0200

    adding header and footer if not logged in

--- matcha/matcha/layouts/empty.vue
+++ matcha/matcha/layouts/empty.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="d-flex flex-column min-vh-100 overflow-hidden">
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+      <!-- Brand -->
+      <b-navbar-brand class="font-weight-bold" to="/">MATCHA</b-navbar-brand>
+    </b-navbar>
+
+    <div class="py-5" style="flex: auto">
+      <Nuxt />
+    </div>
+
+    <div class="position-relative">
+      <div class="bg-primary text-white text-center text-lg-start mt-auto">
+        <div class="container p-4">
+          <p>Brought by</p>
+          <div id="name-wrapper">
+            <h5 class="text-uppercase p-2">MANDRIC</h5>
+            <h5 class="text-uppercase p-2">RMAZARS</h5>
+            <h5 class="text-uppercase p-2">TMARCON</h5>
+            <h5 class="text-uppercase p-2">JOBENASS</h5>
+          </div>
+        </div>
+
+        <div
+          class="text-center p-3"
+          style="background-color: rgba(0, 0, 0, 0.2)"
+        >
+          © 2022 Copyright:
+          <a class="text-white" href="#">42 MATCHA</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+#name-wrapper {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+}
+</style>
