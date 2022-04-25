e1b0378f519f055b426998fef3261b8a8869a573
RobinMazars <45461136+RobinMazars@users.noreply.github.com>
2022-04-25 12:33:47 +0200

    favicon + title + google auth btn

--- matcha/matcha/nuxt.config.js
+++ matcha/matcha/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'matcha-front',
+    title: 'matcha',
     htmlAttrs: {
       lang: 'en',
     },
